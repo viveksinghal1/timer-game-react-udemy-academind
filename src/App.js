5aa6c4c6f17fbf15ea402c58d5f9ed45882ea0d0
Vivek Singhal <48623606+viveksinghal1@users.noreply.github.com>
2024-08-10 11:26:29 +0000

    feat: added TimerChallenge components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Player from "./components/Player";
+import TimerChallenge from "./components/TimerChallenge";
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title="Easy" targetTime="1" />
+        <TimerChallenge title="Not easy" targetTime="5" />
+        <TimerChallenge title="Getting Hard" targetTime="10" />
+        <TimerChallenge title="Going Pro" targetTime="15" />
+      </div>
     </>
   );
 }
