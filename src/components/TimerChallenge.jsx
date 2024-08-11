48e19878598598302b78af4c759584b1a295d665
Vivek Singhal <48623606+viveksinghal1@users.noreply.github.com>
2024-08-11 09:52:03 +0000

    feat: useImerativeHandle

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -11,7 +11,7 @@ export default function TimerChallenge({ title, targetTime }) {
     timer.current = setTimeout(() => {
       setTimerExpired(true);
       setTimerStarted(false);
-      dialog.current.showModal();
+      dialog.current.open();
     }, targetTime * 1000);
     setTimerStarted(true);
   }
